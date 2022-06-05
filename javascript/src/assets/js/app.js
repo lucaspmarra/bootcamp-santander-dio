509e10800c104f4bcd748d5a753b84c6c9a4c05e
Lucas Marra <marralucas1@gmail.com>
2022-06-05 17:11:39 -0300

    create of todo list page

--- javascript/src/assets/js/app.js
+++ javascript/src/assets/js/app.js
@@ -20,7 +20,7 @@ function returnEvenValues(array) {
 let array = [1, 2, 3, 4, 5, 6, 7, 9, 10]
 returnEvenValues(array)
 
-
+// Counter
 var currentNumberWrapper = document.getElementById("currentNumber");
 var currentNumber = 0;
 function increment() {
@@ -30,4 +30,35 @@ function increment() {
 function decrement() {
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
+}
+
+const form = document.getElementById('task-form');
+const taskList = document.getElementById('tasks');
+
+form.onsubmit = function (e) {
+	e.preventDefault();
+	const inputField = document.getElementById('task-input');
+	addTask(inputField.value);
+	form.reset();
+};
+
+// To-Do List
+function addTask(description) {
+	const taskContainer = document.createElement('div');
+	const newTask = document.createElement('input');
+	const taskLabel = document.createElement('label');
+	const taskDescriptionNode = document.createTextNode(description);
+
+	newTask.setAttribute('type', 'checkbox');
+    newTask.setAttribute('name', description);
+	newTask.setAttribute('id', description);
+
+	taskLabel.setAttribute('for', description);
+	taskLabel.appendChild(taskDescriptionNode);
+
+	taskContainer.classList.add('task-item');
+	taskContainer.appendChild(newTask);
+	taskContainer.appendChild(taskLabel);
+
+	taskList.appendChild(taskContainer);
 }
