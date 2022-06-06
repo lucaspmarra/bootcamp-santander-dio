47826a1dcd3ebee7013edf285eb862597ef5c880
Lucas Paulino Marra <lucas.marra.terceirizado@tre-go.jus.br>
2022-06-06 14:51:54 -0300

    java update

--- java/src/br/com/dio/firstProgram.java
+++ java/src/br/com/dio/firstProgram.java
@@ -2,6 +2,10 @@ package br.com.dio;
 
 import br.com.dio.model.Cat;
 
+
+import java.util.Scanner;
+import java.util.Scanner;
+
 public class firstProgram {
     public static void main(String[] args) {
         Cat cat = new Cat("Uni", "tricolor", 1);
@@ -40,5 +44,45 @@ class Book {
     @Override
     public String toString() {
         return "Book{" + "name='" + name + '\'' + ", numPages=" + numPages + '}';
+    }
+}
+
+
+class calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a, b;
+
+        System.out.println("Digite o primeiro valor: ");
+        a = input.nextInt();
+        System.out.println("\nDigite o segundo valor: ");
+        b = input.nextInt();
+
+        int sum = sum(a, b);
+        int sub = sub(a, b);
+        int mul = mul(a, b);
+        double div = div(a, b);
+
+        System.out.println("Soma: " + sum);
+        System.out.println("Subtração: " + sub);
+        System.out.println("Multiplicação: " + mul);
+        System.out.println("Divisão: " + div);
+
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+
+    public static int div(int a, int b) {
+        return a / b;
     }
 }
