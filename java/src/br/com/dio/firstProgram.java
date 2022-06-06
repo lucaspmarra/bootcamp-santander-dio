1a1c2a6f5ea04e1b36e8ff78d634a0ba6b414dfd
Lucas Paulino Marra <lucas.marra.terceirizado@tre-go.jus.br>
2022-06-06 09:11:10 -0300

    update on java project

---
+++ java/src/br/com/dio/firstProgram.java
@@ -0,0 +1,11 @@
+package br.com.dio;
+
+import br.com.dio.model.Cat;
+
+public class firstProgram {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+
+        System.out.println(cat);
+    }
+}
---
+++ java/src/br/com/dio/model/Cat.java
@@ -0,0 +1,64 @@
+package br.com.dio.model;
+
+import java.util.Objects;
+
+public class Cat {
+    private String name;
+    private String color;
+    private Integer age;
+
+    public Cat() {
+    }
+
+    public Cat(String name, String color, Integer age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return Objects.equals(name, cat.name) && Objects.equals(color, cat.color) && Objects.equals(age, cat.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, color, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
